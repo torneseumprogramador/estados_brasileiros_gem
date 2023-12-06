ff0e37320b60957acb67fd614aa7bc38679fc7fb
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-12-06 10:48:08 -0300

    fix

--- lib/estados_brasileiros_active_record/generators/install_generator.rb
+++ lib/estados_brasileiros_active_record/generators/install_generator.rb
@@ -2,15 +2,13 @@ require 'rails/generators'
 module EstadosBrasileirosActiveRecord
     module Generators
         class InstallGenerator < Rails::Generators::Base
-            source_root File.expand_path('../estados_brasileiros_active_record', __dir__)
-
             def copy_rake_file
-                rake_file = 'tasks/importacao_estados.rake'
+                rake_file = File.expand_path('../estados_brasileiros_active_record/tasks/importacao_estados.rake', __dir__)
                 copy_file rake_file, "lib/tasks/importacao_estados.rake"
             end
             
             def copy_model_file
-                model_file = 'models/estado.rb'
+                model_file = File.expand_path('../estados_brasileiros_active_record/models/estado.rb', __dir__)
                 copy_file model_file, "app/models/estados_brasileiros_active_record/estado.rb"
             end
         end
