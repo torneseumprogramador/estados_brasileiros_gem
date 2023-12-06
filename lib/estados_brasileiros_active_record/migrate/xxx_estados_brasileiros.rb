0a8d22940b5782f29addac2503bf06eab69fe965
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-12-06 11:10:28 -0300

    fix

--- lib/estados_brasileiros_active_record/migrate/xxx_estados_brasileiros.rb
+++ lib/estados_brasileiros_active_record/migrate/xxx_estados_brasileiros.rb
@@ -1,11 +1,9 @@
-module EstadosBrasileirosActiveRecord
-  class CreateEstados < ActiveRecord::Migration[6.0]
-    def change
-      unless table_exists?(:estados)
-        create_table :estados do |t|
-          t.string :nome
-          t.string :sigla
-        end
+class CreateEstados < ActiveRecord::Migration[6.0]
+  def change
+    unless table_exists?(:estados)
+      create_table :estados do |t|
+        t.string :nome
+        t.string :sigla
       end
     end
   end
