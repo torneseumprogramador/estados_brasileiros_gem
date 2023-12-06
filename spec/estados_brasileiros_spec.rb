3e69262d7b5164de5acd839743354dfb6dcedc5f
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-12-06 11:17:28 -0300

    fix

--- spec/estados_brasileiros_spec.rb
+++ spec/estados_brasileiros_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe EstadosBrasileiros do
   it "has a version number" do
     expect(EstadosBrasileiros::VERSION).not_to be nil
   end
+
+  it "loads the Railtie into Rails" do
+    railtie = Rails::Railtie.subclasses.find { |rt| rt.to_s == 'EstadosBrasileiros::Railtie' }
+    expect(railtie).not_to be_nil
+  end
 end
